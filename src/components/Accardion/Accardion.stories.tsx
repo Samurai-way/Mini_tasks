0954275f64a0a00e0c02edffb2a4b5674385dfd8
Samurai-way <olegmoishevych@gmail.com>
2022-08-22 21:40:24 +0300

    action ('want to change')

--- src/components/Accardion/Accardion.stories.tsx
+++ src/components/Accardion/Accardion.stories.tsx
@@ -1,14 +1,17 @@
 import React, {useState} from 'react';
 import {Accardion} from './Accardion';
+import {action} from "@storybook/addon-actions";
 
 export default {
     title: 'Accardion',
     component: Accardion,
 }
 
+let CallBack = action('want to change')
+
 export const AccardionFalse = () => <Accardion collapsed={false}
                                                titleValue={'Title list'}
-                                               onClick={()=>{}}
+                                               onClick={CallBack}
                                                color={'red'}
 />
 export const AccardionTrue = () => <Accardion collapsed={true}
