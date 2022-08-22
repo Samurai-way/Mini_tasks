bc39206d84b31a5abcd433aa1dcc5ec09889d715
Samurai-way <olegmoishevych@gmail.com>
2022-08-22 21:19:26 +0300

    Storybook Accardion

---
+++ src/components/Accardion/Accardion.stories.tsx
@@ -0,0 +1,25 @@
+import React, {useState} from 'react';
+import {Accardion} from './Accardion';
+
+export default {
+    title: 'Accardion',
+    component: Accardion,
+}
+
+export const AccardionFalse = () => <Accardion collapsed={false}
+                                               titleValue={'Title list'}
+                                               onClick={()=>{}}
+                                               color={'red'}
+/>
+export const AccardionTrue = () => <Accardion collapsed={true}
+                                               titleValue={'Title list'}
+                                               onClick={()=>{}}
+                                               color={'red'}
+/>
+export const AccardionDefault = () => {
+    let [call, setCall]=useState<boolean>(true)
+    return <Accardion collapsed={call} titleValue={'Title list'} onClick={() => setCall(!call)} color={'red'}/>
+}
+
+
+
