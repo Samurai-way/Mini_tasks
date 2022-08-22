362aeef3f07ad4bf23a069c5ea28bb679888b828
Samurai-way <olegmoishevych@gmail.com>
2022-08-22 21:42:31 +0300

    action ('want to onn or off')

--- src/components/Onn/OnnOff.stories.tsx
+++ src/components/Onn/OnnOff.stories.tsx
@@ -1,29 +1,20 @@
 import React, {useState} from 'react';
 import {OnnOff} from "./Off";
+import {action} from "@storybook/addon-actions";
 
 export default {
     title: 'OnnOff',
     component: OnnOff,
 }
 
-export const Onn = () => <OnnOff on={true} setOnn={()=>{}}/>
+let OnClick = action('want to ONN or OFF')
 
-export const Off = () => <OnnOff on={false} setOnn={()=>{}}/>
+export const Onn = () => <OnnOff on={true} setOnn={OnClick}/>
+
+export const Off = () => <OnnOff on={false} setOnn={OnClick}/>
 
 export const OffOnn = () => {
     let [on, setON]=useState<boolean>(false)
     return <OnnOff on={on} setOnn={()=>{setON(!on)}}/>
 }
 
-
-// export const OnnOff = () => {
-//
-//     let [call, setCall]=useState<boolean>(true)
-//
-//     return <Accardion
-//         collapsed={call}
-//         titleValue={'Title list'}
-//         onClick={() => setCall(!call)}
-//         color={'red'}
-//     />
-// }
