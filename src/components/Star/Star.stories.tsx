02f7d167a33078fba30e27e78108c20cbe0037c7
Samurai-way <olegmoishevych@gmail.com>
2022-08-22 21:43:24 +0300

    action ('want to change 1 - 4')

--- src/components/Star/Star.stories.tsx
+++ src/components/Star/Star.stories.tsx
@@ -1,16 +1,19 @@
 import React, {useState} from 'react';
 import {AppProps, Father} from "./Star";
+import {action} from "@storybook/addon-actions";
 
 export default {
     title: 'Star',
     component: Father,
 }
 
-export const Star0 = () => <Father value={0} setValue={()=>{}}/>
-export const Star1 = () => <Father value={1} setValue={()=>{}}/>
-export const Star2 = () => <Father value={2} setValue={()=>{}}/>
-export const Star3 = () => <Father value={3} setValue={()=>{}}/>
-export const Star4 = () => <Father value={4} setValue={()=>{}}/>
+let OnClick = action('want to change 1 - 4')
+
+export const Star0 = () => <Father value={0} setValue={OnClick}/>
+export const Star1 = () => <Father value={1} setValue={OnClick}/>
+export const Star2 = () => <Father value={2} setValue={OnClick}/>
+export const Star3 = () => <Father value={3} setValue={OnClick}/>
+export const Star4 = () => <Father value={4} setValue={OnClick}/>
 
 export const StarDefault = () => {
     let [value, setValue]=useState<AppProps>(0)
