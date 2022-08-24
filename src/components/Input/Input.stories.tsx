b9f43c9784fabc8e5033188e0c5bea4918bd00b1
Samurai-way <olegmoishevych@gmail.com>
2022-08-24 08:38:46 +0300

    Get Value change

--- src/components/Input/Input.stories.tsx
+++ src/components/Input/Input.stories.tsx
@@ -8,10 +8,10 @@ export const UncontrolledInput = () => <input/>
 
 export const TrackValueUncontrolledInput = () => {
 
-    const [value, setValue]=useState('')
+    const [value, setValue] = useState('')
 
     return <>
-        <input onChange={(event)=>{
+        <input onChange={(event) => {
             const eventValue = event.currentTarget.value;
             setValue(eventValue)
         }}/> - {value}
@@ -19,16 +19,16 @@ export const TrackValueUncontrolledInput = () => {
 }
 
 export const GetValue = () => {
-    const [value, setValue]=useState('')
-    const inputRef = useRef<HTMLInputElement>(null)
 
-    const save = () => {
-        const el = inputRef.current as HTMLInputElement;
+    const [value, setValue]=useState('')
+    const ref = useRef<HTMLInputElement>(null)
+
+    const onClickButton = () => {
+        const el = ref.current as HTMLInputElement;
         setValue(el.value);
     }
-
     return <>
-        <input ref={inputRef}/>
-        <button onClick={save}>save</button> - actual value: {value}
+        <input ref={ref}/>
+        <button onClick={onClickButton}> save </button> -- newValue: {value}
     </>
 }
