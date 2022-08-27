a8925ba49670b4cde570ffb222e95a1c05145af0
Samurai-way <olegmoishevych@gmail.com>
2022-08-27 08:11:30 +0300

    change city

--- src/components/test/test.props.ts
+++ src/components/test/test.props.ts
@@ -21,5 +21,28 @@ export type heirType = {
     name: string,
     age: number,
     heir: number,
-    address: {title: string}
+    address: {city: string, house: number}
+}
+
+export type LeptopType = {
+    title: string
+}
+
+export type UserWithLeptopType = heirType &{
+    laptop: LeptopType
+}
+
+export function inc(person: heirType, power: number) {
+    let copy = {...person}
+    copy.heir = person.heir / power
+
+    return copy
+}
+
+export function leptop(person: UserWithLeptopType, city: string) {
+    let copy = {...person}
+
+    copy.address = {...person.address, city: 'USA', house: 10}
+
+    return copy
 }
