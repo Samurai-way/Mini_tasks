1a9fc22a6cf0c0176547b8b1e1719bb82d1a4abe
Samurai-way <olegmoishevych@gmail.com>
2022-08-27 08:56:23 +0300

    new Copy user => 'Canada'

--- src/components/test/test.props.ts
+++ src/components/test/test.props.ts
@@ -17,39 +17,55 @@
 //     heir: number
 // }
 
-export type heirType = {
+// export type heirType = {
+//     name: string,
+//     age: number,
+//     heir: number,
+//     address: {city: string, house: number}
+// }
+//
+// export type LeptopType = {
+//     title: string
+// }
+//
+// export type UserWithLeptopType = heirType &{
+//     laptop: LeptopType
+// }
+//
+// export function inc(person: heirType, power: number) {
+//     let copy = {...person}
+//     copy.heir = person.heir / power
+//
+//     return copy
+// }
+//
+// export function leptop(person: UserWithLeptopType, city: string) {
+//     let copy = {...person,
+//         address: {
+//             city
+//         }}
+//
+//     return copy
+// }
+//
+// export function leptopUserMacbook(person: UserWithLeptopType, title: string) {
+//     return {
+//         ...person, laptop: {...person.laptop, title: title}
+//     }
+// }
+
+
+
+export type UserType = {
     name: string,
     age: number,
-    heir: number,
-    address: {city: string, house: number}
+    address: { title: string },
+    technologies: { title: string }
 }
 
-export type LeptopType = {
-    title: string
-}
-
-export type UserWithLeptopType = heirType &{
-    laptop: LeptopType
-}
-
-export function inc(person: heirType, power: number) {
-    let copy = {...person}
-    copy.heir = person.heir / power
-
-    return copy
-}
-
-export function leptop(person: UserWithLeptopType, city: string) {
-    let copy = {...person,
-        address: {
-            city
-        }}
-
-    return copy
-}
-
-export function leptopUserMacbook(person: UserWithLeptopType, title: string) {
-    return {
-        ...person, laptop: {...person.laptop, title: title}
+export function changeUser(user: UserType, title: string){
+    return{
+        ...user,
+        address: {...user.address, title: title}
     }
 }
