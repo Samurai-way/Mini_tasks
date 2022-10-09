ef7643f15d55ef2f4e852815f3714964299294f6
Samurai-way <olegmoishevych@gmail.com>
2022-10-09 18:43:24 +0300

    storybook, useEffect, clearTimeout

--- src/stories/UseState.stories.tsx
+++ src/stories/UseState.stories.tsx
@@ -1,4 +1,4 @@
-import React, {useMemo, useState} from 'react';
+import React, {useEffect, useMemo, useState} from 'react';
 
 export default {
     title: 'useState/useState'
@@ -15,8 +15,37 @@ export const Example = () => {
 
     const [value, setValue] = useState(exam)
 
+    useEffect(()=>{
+        console.log('click')
+       return () => {
+           console.log('reset clock')
+       }
+    },[value])
+
     return <>
         <h3>{value}</h3>
         <button onClick={() => setValue(value => value+1)}>inc</button>
+    </>
+}
+
+export const Input = () => {
+
+    const [value, setValue] = useState('')
+
+    useEffect(()=>{
+        const timerID = setTimeout(()=>{
+            console.log('new text')
+            setValue('new text')
+        }, 2000)
+        window.addEventListener('keypress', (e)=>{
+            setValue((state)=>state + e.key)
+        })
+        return ()=>{
+            clearTimeout(timerID)
+        }
+    },[])
+
+    return <>
+        <h1>Typed text: {value}</h1>
     </>
 }
